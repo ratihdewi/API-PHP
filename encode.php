b3cdac7bb665d57277970e6fc87fea4da32831c1
ratihdewi <33591606+ratihdewi@users.noreply.github.com>
2018-08-31 09:35:57 +0700

    Update encode.php

--- encode.php
+++ encode.php
@@ -19,12 +19,12 @@
 				$arrayy = array('jumlah' => 0);	
 				echo json_encode($arrayy);
 			}			
-		}elseif(Del_Inflection_Suffixes($kata) == 2){
+		}elseif(Del_Inflection_Suffixes($kata) == 1){
 			$kataAsal = $kata;
 			if(preg_match('/([km]u|nya|[kl]ah|pun)$/i',$kata)){ // Cek Inflection Suffixes
 				$__kata = preg_replace('/([km]u|nya|[kl]ah|pun)$/i','',$kata);		
-				if(preg_match('/([klt]ah|pun)$/i',$kata)){ // Jika berupa particles (“-lah”, “-kah”, “-tah” atau “-pun”)	
-					if(preg_match('/([km]u|nya)$/i',$__kata)){ // Hapus Possesive Pronouns (“-ku”, “-mu”, atau “-nya”)
+				if(preg_match('/([klt]ah|pun)$/i',$kata)){ // Jika berupa particles (â€œ-lahâ€, â€œ-kahâ€, â€œ-tahâ€ atau â€œ-punâ€)	
+					if(preg_match('/([km]u|nya)$/i',$__kata)){ // Hapus Possesive Pronouns (â€œ-kuâ€, â€œ-muâ€, atau â€œ-nyaâ€)
 						$__kata__ = preg_replace('/([km]u|nya)$/i','',$__kata);
 						$__kata__ = $kata1;
 						$kata1 = array('jumlah' => 1,'kembalian'=>'ada');
@@ -38,28 +38,7 @@
 			
 			echo json_encode ($kataAsal);			
 		
-		} elseif(Del_Derivation_Suffixes($kata) ==1){
-			$kataAsal = $kata;
-			
-			if(preg_match('/(i|an)$/i',$kata)){ // Cek Suffixes
-				$__kata = preg_replace('/(i|an)$/i','',$kata);		
-			if(cekKamus($__kata)){ // Cek Kamus			
-				return $__kata;
-			}
-			/*-- Jika Tidak ditemukan di kamus --*/
-			if(preg_match('/(kan)$/i',$kata)){ // cek -kan 				
-				$__kata__ = preg_replace('/(kan)$/i','',$kata);
-				if(cekKamus($__kata__)){ // Cek Kamus
-					return $__kata__;
-				}
-			}
-			if(Cek_Prefix_Disallowed_Sufixes($kata)){
-				return $kataAsal;
-			}
-		
-		}
-		return $kataAsal;
-		}
+		} 
 		
 	
 ?>
